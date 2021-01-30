51d33951e27d5b709c696c2a5dbc8ccfcf98b5f0
rroyalty <rroyalty@gmail.com>
2021-01-30 12:50:50 -0500

    Javascript

--- ryanjsfolder/ryan.js
+++ ryanjsfolder/ryan.js
@@ -9,21 +9,9 @@ $(document).ready(function () {
     const lookUpURL = "lookup.php?i=";
     const searchURL = "search.php?s=";
 
-    // The JSON object that our final list of IDs will push to.
-    let finalList = { "items": [] };
-    // let typeAPI = "drink"
-
-    // Experimental array of ingredients
-
-    // Call get Ingredient Promises
-    // getIngredPromises(ingredients, typeAPI);
 
     // get Ingredient Promises function
-    function getIngredPromises(array, type) {
-
-        if (type === "meal") functionURL = mealURL;
-        else if (type === "drink") functionURL = drinkURL;
-        else return;
+    function getIngredPromises(array, type, arrGen, counts, finalList, functionURL) {
 
         // Created an array of promises, each index being the response for one of the ingredients in the array.
         for (let i = 0; i < array.length; i++) {
@@ -33,12 +21,7 @@ $(document).ready(function () {
         // Waits for all promises, then generates a list of IDs for the drinks that use the ingredients. Sorts the list by most ingredients found to least.
         Promise.all(array).then((response) => {
 
-            // Array used to concat all of the drinks found for each ingredient provided.
-            let arrGen = [];
-
-            // Object used to count the number of times that each meal appears in the concat list.
-            let counts = {};
-
+            console.log(response)
             // Loop to concat the arrays of IDs together.
             for (let i = 0; i < response.length; i++) {
                 if (type === "drink") { arrGen = arrGen.concat(response[i].drinks.map(function (v) { return v.idDrink })) }
@@ -50,35 +33,51 @@ $(document).ready(function () {
                 counts[x] = (counts[x] || 0) + 1;
             })
 
+            console.log(finalList);
             // Converts the resulting counts object into a more easily usable JSON object.
             Object.entries(counts).forEach(e => finalList["items"].push({ "id": e[0], "count": e[1] }))
+            console.log(finalList);
 
             // Sorts the JSON object by count descending.
             finalList.items.sort(function (a, b) { return b.count - a.count });
 
-            // // Top 10.
-            // let top10 = [];
-            // for (let i = 0; i < 10 || i < finalList.length; i++) { top10[i] = $.get(functionURL + lookUpURL + finalList.items[i].id, ((response) => { return response })) };
-            // Promise.all(top10).then((response) => { console.log(response) });
+            // Top 10.
+            let top10 = [];
+            for (let i = 0; i < 10 || i < finalList.length; i++) { top10[i] = $.get(functionURL + lookUpURL + finalList.items[i].id, ((response) => { return response })) };
+            Promise.all(top10).then((response) => { console.log(response) });
+
+            console.log(top10)
             
             let drinkID = finalList.items[Math.floor(Math.random() * finalList.items.length)].id;
-
+            if ( type === "drink") {
             $.get(functionURL + lookUpURL + drinkID, function(response) { 
-                console.log( response);
                 $("#drinkTitle").text(response.drinks[0].strDrink);
                 $("#drinkImg").attr("src", response.drinks[0].strDrinkThumb); 
-            });
+            }); } else if ( type === "meal") {
+                $.get(functionURL + lookUpURL + drinkID, function(response) {
+                    $("#mealTitle").text(response.meals[0].strMeal);
+                    $("#mealImg").attr("src", response.meals[0].strMealThumb); 
+            })}
 
         })
     };
 
     $('#ryansButton').on('click', function (event) {
         event.preventDefault;
-        let selection = $('#drinks').val();
-        console.log(selection);
-        let ingredients = [selection];
-        getIngredPromises(ingredients, "drink");
 
+        let drinkFinalList = { "items": [] };
+        let arrGenDrink = [];
+        let countsDrink = {};
+        let drinkSelection = $('#drinks').val();
+        let drinkIngredients = [drinkSelection];
+        getIngredPromises(drinkIngredients, "drink", arrGenDrink, countsDrink, drinkFinalList, drinkURL);
+
+        let mealFinalList = { "items": [] };
+        let arrGenMeal = [];
+        let countsMeal = {};
+        let mealSelection = $('#meals').val();
+        let mealIngredients = [mealSelection];
+        getIngredPromises(mealIngredients, "meal", arrGenMeal, countsMeal, mealFinalList, mealURL);
 
     });
 
