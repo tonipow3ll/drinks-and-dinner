07c241a5665de6c5044711130f8d77da6a43e070
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-01-31 15:48:48 -0500

    Add Owl Carousel and images to index

--- tabs.js
+++ tabs.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+  // Tabs functionality
     $('#tabs li').on('click', function() {
       var tab = $(this).data('tab');
   
@@ -8,4 +10,30 @@ $(document).ready(function() {
       $('#tab-content p').removeClass('is-active');
       $('p[data-content="' + tab + '"]').addClass('is-active');
     });
+
+  // Owl Carousel
+  $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        // margin:10,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        nav:false,
+        responsiveClass:true,
+        responsive:{
+          0:{
+              items:1,
+          },
+          600:{
+              items:1,
+          },
+          1000:{
+              items:1
+          }
+        }
+        // slideSpeed : 300,
+        // paginationSpeed : 400
+      });  
+
   });
