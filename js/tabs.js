ed3162ce1179e3415a7f2857f99118e14c3b5692
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-02-01 19:44:04 -0500

    Added owl carousel

--- js/tabs.js
+++ js/tabs.js
@@ -8,4 +8,30 @@ $(document).ready(function() {
       $('#tab-content p').removeClass('is-active');
       $('p[data-content="' + tab + '"]').addClass('is-active');
     });
+
+     // Owl Carousel
+     $('.owl-carousel').owlCarousel({
+      items:1,
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:4000,
+      autoplayHoverPause:true,
+      autoHeight:true,
+      nav:false,
+      responsiveClass:true,
+      responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1
+        }
+      }
+      
+    });  
+
+
   });
