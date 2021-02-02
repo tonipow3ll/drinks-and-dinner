9ca0454a437142476ab7bdf83766189456141b01
rroyalty <rroyalty@gmail.com>
2021-02-01 21:39:17 -0500

    Tabs stuff

--- js/script.js
+++ js/script.js
@@ -328,15 +328,15 @@ $(document).ready(function () {
         $("#recipes").removeClass("is-active")
     })
 
-    $('#tabs li').on('click', function () {
-        var tab = $(this).data('tab');
+    // $('#tabs li').on('click', function () {
+    //     var tab = $(this).data('tab');
 
-        $('#tabs li').removeClass('is-active');
-        $(this).addClass('is-active');
+    //     $('#tabs li').removeClass('is-active');
+    //     $(this).addClass('is-active');
 
-        $('#tab-content p').removeClass('is-active');
-        $('p[data-content="' + tab + '"]').addClass('is-active');
-    });
+    //     $('#tab-content p').removeClass('is-active');
+    //     $('p[data-content="' + tab + '"]').addClass('is-active');
+    // });
 
 // // START JON JS!!!!
 // ​
